e5a925f1ecc73b542750251c07978e588608ca71
sgnl <rayrfarias@gmail.com>
2017-02-04 22:00:43 -1000

    🚧 - stubs out form

--- pages/new.js
+++ pages/new.js
@@ -3,6 +3,35 @@ import Layout from '../components/layout'
 
 export default () => {
   return (
-    <Layout><h2>New Card!</h2></Layout>
+    <Layout>
+      <form>
+        <label>
+          name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          bio:
+          <textarea name="bio">
+          </textarea>
+        </label>
+        <label>
+          tags:
+          <input type="text" name="tags" />
+        </label>
+        <label>
+          websites:
+          <input type="text" name="websites" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </Layout>
   )
 }
+
+/*
+  name
+  bio
+  genres : [3]
+  websites [ [k, v], ... ] => k = "social-media-nick", v = "url"
+
+ */
